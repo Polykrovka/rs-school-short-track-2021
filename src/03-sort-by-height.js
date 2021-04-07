f3e60ee10d917877b5d8409b0be6d0505dbdcc45
Polykrovka <2595092@gmail.com>
2021-04-08 00:13:11 +0300

    feat: add solytion 03 task

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,23 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const sort = arr.slice();
+  sort.sort((a, b) => {
+    if (a > b) return 1; // если первое значение больше второго
+    if (a === b) return 0; // если равны или если значение -1
+    if (a < b) return -1; // если первое значение меньше второго
+    return 0;
+  });
+  while (sort[0] === -1) {
+    sort.shift();
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      sort.splice(i, 0, -1);
+    }
+  }
+  return sort;
 }
 
 module.exports = sortByHeight;
