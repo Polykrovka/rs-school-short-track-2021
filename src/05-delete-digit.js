02c5ea731c25e0b2b0028e71205d453c652a81bb
Polykrovka <2595092@gmail.com>
2021-04-09 18:38:43 +0300

    feat: add solytion 05 task

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,20 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arrDigits = n.toString().split('');
+  const allDigit = [];
+  let digit = '';
+  for (let del = 0; del < arrDigits.length; del++) {
+    for (let i = 0; i < arrDigits.length; i++) {
+      if (i !== del) {
+        digit += arrDigits[i];
+      }
+    }
+    allDigit.push(Number(digit));
+    digit = '';
+  }
+  return Math.max(...allDigit);
 }
 
 module.exports = deleteDigit;
