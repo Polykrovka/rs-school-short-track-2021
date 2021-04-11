ce662e3d373bd6bffbdd947cf2594550a80151cb
Polykrovka <2595092@gmail.com>
2021-04-11 10:27:44 +0300

    feat: add solytion 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let counter = 0;
+  const arrStr = str.split('');
+  let answer = '';
+  let previous = arrStr[0];
+  for (let i = 0; i < arrStr.length + 1; i++) {
+    if (arrStr[i] === previous) {
+      counter += 1;
+    } else {
+      if (counter > 1) {
+        answer += `${counter}${previous}`;
+      } else {
+        answer += previous;
+      }
+      counter = 1;
+      previous = arrStr[i];
+    }
+  }
+  return answer;
 }
 
 module.exports = encodeLine;
