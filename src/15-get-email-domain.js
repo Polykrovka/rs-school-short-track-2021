a08bf80d975338b70a7e3522bc7d827d8ee209a7
Polykrovka <2595092@gmail.com>
2021-04-11 12:17:09 +0300

    feat: add solytion 15 task

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const dividedEmail = email.split('@').reverse();
+  return dividedEmail[0];
 }
 
 module.exports = getEmailDomain;
